f639f31647aa469883d6556d4d2e7521443f50be
prashantvaid <92187498+prashantvaid@users.noreply.github.com>
2025-02-15 17:17:13 -0800

    eee

---
+++ utils/supabase/server.ts
@@ -0,0 +1,15 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { Database } from '@/types/supabase';
+
+export const createClient = () => {
+  if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+    throw new Error('Missing Supabase environment variables');
+  }
+  
+  return createServerComponentClient<Database>({ 
+    cookies,
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  });
+} 
